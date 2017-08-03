8a7203b63dee8f3f036e7e2618c2b6e8732fe88f
jknees <cortezcardinal@gmail.com>
2017-08-03 16:18:28 -0700

    Added GL window class

---
+++ ProjectFiles/SandboxGame/MyGLWindow.h
@@ -0,0 +1,8 @@
+#ifndef SANDBOX_MY_GL_WINDOW_H
+#define SANDBOX_MY_GL_WINDOW_H
+#include <QtOpenGL\qgl.h>
+class MyGLWindow : public QGLWidget
+{
+};
+
+#endif
--- ProjectFiles/SandboxGame/SandboxGame.cpp
+++ ProjectFiles/SandboxGame/SandboxGame.cpp
@@ -1,9 +1,12 @@
 #include <QtWidgets\qapplication.h>
+#include <QtWidgets\qwidget.h>
+#include "MyGLWindow.h"
 
 int main(int argc, char* argv[])
 {
 	QApplication application(argc, argv);
-
+	MyGLWindow myGlWindow;
+	myGlWindow.show();
 	return application.exec();
 }
 
